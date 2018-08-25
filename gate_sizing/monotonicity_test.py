29957dc25d509f721d0a90e3d78db16cafa31100
Lucas Slot <lfh.slot@gmail.com>
2018-08-25 21:31:40 +0200

    update

--- gate_sizing/monotonicity_test.py
+++ gate_sizing/monotonicity_test.py
@@ -9,10 +9,9 @@ import random
 import itertools
 import copy
 import numpy as np
+import sys
 
-def double_monotone(L):
-    i = np.argmin(L)
-    return monotone(L[:i]) and monotone(L[i:])
+MAX_INT = 10000000000
 
 def monotone(L):
     return non_increasing(L) or non_decreasing(L)
@@ -23,29 +22,63 @@ def non_increasing(L):
 def non_decreasing(L):
     return all(x<=y for x, y in zip(L, L[1:]))
 
+def count_shifts(L):
+    shifts = 0
+    
+    def get_dir(i):
+        if L[i] < L[i+1]:
+            direction = 1
+        elif L[i] > L[i+1]:
+            direction = -1
+        else:
+            direction = 0
+        return direction
+
+    direction = get_dir(0)
+
+    for i in xrange(1, len(L) - 1):
+        new_direction = get_dir(i)
+        if new_direction * direction == -1:
+            shifts += 1
+        if new_direction != 0:
+            direction = new_direction
+    return shifts
+
 def delay(s, load=0.0):
     d = 0.0
     for i in range(len(s) - 1):
         d += float(s[i + 1]) / s[i]
-    d += load / s[-1]
+    d += float(load) / s[-1]
     return d
     
-def best_order(s, load=0.0):
-    min_delay = delay(s, load)
+def best_order(s, load=0.0, fixed_first=False):
+    min_delay = MAX_INT
     best_order = copy.deepcopy(s)
-    for order in itertools.permutations(s):
-        cur_delay = delay(order, load)
-        if cur_delay <= min_delay:
-            min_delay = cur_delay
-            best_order = copy.deepcopy(order)
+
+    if fixed_first:
+        for porder in itertools.permutations(s[1:]):
+            order = [s[0]] + list(porder)
+            cur_delay = delay(order, load)
+            if cur_delay <= min_delay:
+                min_delay = cur_delay
+                best_order = copy.deepcopy(order)
+    else:
+        for order in itertools.permutations(s):
+            cur_delay = delay(order, load)
+            if cur_delay <= min_delay:
+                min_delay = cur_delay
+                best_order = copy.deepcopy(order)
+
     return best_order, min_delay
     
-n = 10
+n = int(sys.argv[1])
+s_max = 10
 while(True):
-    s = [random.randrange(1, 100) for i in range(n)]
-    load = random.randrange(1, 100)
-    L = best_order(s, load)[0]
-    dm = double_monotone(L)
-    print L, load, dm
-    if not dm:
-        exit()
+    s = [random.randrange(1, s_max) for i in range(n)]
+    load = random.randrange(s_max, 2 * s_max)
+    L = best_order(s, load, True)[0]
+    shifts = count_shifts(L)
+    m = monotone(L)
+    print L, load, shifts
+    if shifts > 2:
+        break
--- gate_sizing/simple_path.py
+++ gate_sizing/simple_path.py
@@ -3,17 +3,24 @@ import random
 import sys
 from math import sqrt
 
+from timeit import default_timer as timer
+
+import cvxopt as cvx
+cvx.solvers.options['show_progress'] = False
+
+import matplotlib.pyplot as plt
+
 DELAY_MIN = 0
 POWER_MIN = 1
 TRADEOFF  = 2
 
-def uniform_l(n):
-    return [1. / n for i in xrange(n)]
+def uniform_l(n, length=1.):
+    return [length / n for i in xrange(n)]
 
 
-def random_l(n):
+def random_l(n, length=1.):
     l = [random.random() for i in range(n)]
-    return [li / sum(l) for li in l]
+    return [length * li / sum(l) for li in l]
 
 
 def check_geometric_averages(s):
@@ -37,7 +44,7 @@ def check_double_monotone(s):
 
 class Path:
     def __init__(self, n):
-        self.length = 1
+        self.length = 1.
         
         self.smin = 1.
         self.smax = 10.
@@ -54,16 +61,34 @@ class Path:
         self.max_delay = 7.        
         self.n = n
 
+
+    def build_path_qp(self, s, mode=DELAY_MIN):
+        q = [self.wire_cap / (self.gate_res * s[i]) + self.wire_res * self.gate_cap * s[i+1] \
+             for i in xrange(self.n - 1)]
+        q = cvx.matrix(q)
+
+        p = [0.5 * self.wire_res * self.wire_cap for i in xrange(self.n - 1)]
+        P = cvx.spdiag(p)
+
+        G = -cvx.spdiag([1. for i in xrange(self.n - 1)])
+        h = cvx.matrix([0. for i in xrange(self.n - 1)])
+
+        A = cvx.matrix([1. for i in xrange(self.n - 1)]).T
+        b = cvx.matrix([self.length])
+
+        return P, q, G, h, A, b
+
         
-    def build_path_ggp(self, prim_in=0, prim_out=0, mode=DELAY_MIN, fixed_l = []):
+    def build_path_ggp(self, prim_in=0, prim_out=0, mode=DELAY_MIN, fixed_l=[], fixed_s=[]):
         s = gp.VectorVariable(self.n, "s")
         l = gp.VectorVariable(self.n - 1, "l")
     
-        delay = self.__get_delay_function(s, l)
-        power = self.__get_power(s)
-
         constraints = []
-        self.__set_s_bounds(s, constraints)
+
+        if not fixed_s:
+            self.__set_s_bounds(s, constraints)
+        else:
+            self.__fix_s(fixed_s, s, constraints)
 
         if not fixed_l:
             self.__set_l_bounds(l, constraints)
@@ -73,6 +98,9 @@ class Path:
 
         self.__set_primary_in_out(s, prim_in, prim_out, constraints)
 
+        delay = self.__get_delay_function(s, l)
+        power = self.__get_power(s)
+
         if mode == DELAY_MIN:
             return gp.Model(delay, constraints), s, l
         elif mode == POWER_MIN:
@@ -80,6 +108,16 @@ class Path:
             return gp.Model(power, constraints), s, l
         else:
             return gp.Model(power + delay, constraints), s, l
+
+
+    # public version of __get_delay_function (that takes fixed s, l).
+    def get_delay(self, fixed_s, fixed_l):
+        return self.__get_delay_function(fixed_s, fixed_l)
+
+
+    def __fix_s(self, fixed_s, s, constraints):
+        for i in range(self.n):
+            constraints.append(s[i] == fixed_s[i])
 
         
     def __fix_l(self, fixed_l, l, constraints):
@@ -94,6 +132,7 @@ class Path:
         return power
 
 
+    # Can take either fixed or variable s, l.
     def __get_delay_function(self, s, l):
         delay = 0
         for i in xrange(self.n - 1):
@@ -110,7 +149,6 @@ class Path:
 
     
     def __set_l_sum(self, l, constraints):
-        tol = 0.001
         l_sum = 0
         for i in range(self.n - 1):
             l_sum += l[i]
@@ -148,37 +186,44 @@ class Path:
 
 def make_run(n):
     p = Path(n)
+
+    # p.length = float(n) / 10
+
+    p.smax = 50
     
     p.wire_res = random.random() * 2.
     p.wire_cap = random.random() * 5.
     
     p.wire_res = 1.
     p.wire_cap = 1.
+    # p.wire_res = 0.
+    # p.wire_cap = 0.
 
     p_in = random.random() * 4 + 1
-    p_out = round(random.random() * 4 + 1, 0)
+    p_out = round(random.random() * 30 + 1, 0)
     
-    p_in = 000
+    # p_in = 000
 
-    p.smax = 10
     p.lmin = 0.00
 
-    f_l = uniform_l(n - 1)
+    # f_l = uniform_l(n - 1, p.length)
+    # f_l = random_l(n - 1)
+    f_l = []
 
     print p.wire_cap, p.wire_res, p_in, p_out
 
     # Compute minimum delay.
     M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = p_in, prim_out = p_out, fixed_l = f_l)
 
-    # sol = M.localsolve(verbosity=0)
-    sol = M.solve(verbosity=0)
+    sol = M.localsolve(verbosity=0)
+    # sol = M.solve(verbosity=0)
 
     min_delay = float(sol["cost"])
     print "Minimum delay is: " + str(min_delay) + " with power " + str(float(sum(sol(s))))
     print "s:     ", sol(s)
     print "l:     ", sol(l)
     print "sum l: ", sum(sol(l))
-    print "mono:  ", check_double_monotone(sol(s))
+    print "mono:  ", check_double_monotone(sol(l))
     # check_geometric_averages(sol(s))    
     print
 
@@ -186,11 +231,175 @@ def make_run(n):
         exit()
 
 
+def alternate(n, m):
+    p = Path(n)
+
+    p.length = float(n)
+
+    fixed_l = random_l(n - 1, p.length)
+
+    print fixed_l
+
+    p_in = 0
+    p_out = 1
+    # p_out = round(random.random() * 30 + 1, 0)
+
+    # p.wire_res = random.random() * 5.
+    # p.wire_cap = random.random() * 10.
+
+    M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = p_in, prim_out = p_out)
+    sol = M.localsolve(verbosity=0)
+    min_delay = float(sol["cost"])
+    print "GGP solution"
+    # print
+    # print "Minimum delay is: " + str(min_delay) + " with power " + str(float(sum(sol(s))))
+    print "s:     ", sol(s)
+    print "l:     ", sol(l)
+    # print "sum l: ", sum(sol(l))
+    # print
+
+    ggp_sol = min_delay
+    ggp_s = [float(si) for si in sol(s)]
+    ggp_l = [float(li) for li in sol(l)]
+    
+    sol_values = []
+
+    epsilon = 0.000000000001
+
+    for i in xrange(m):
+        # print "Iteration", i
+
+        print min_delay, "opt l", fixed_l
+        M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = p_in, prim_out = p_out, fixed_l = fixed_l)
+        sol = M.solve(verbosity=0)
+        min_delay = float(sol["cost"])
+        # print "Optimised s"
+        # print "Minimum delay is: " + str(min_delay) + " with power " + str(float(sum(sol(s))))
+        # print "s:     ", sol(s)
+        # print "l:     ", sol(l)
+        # print "sum l: ", sum(sol(l))
+
+        sol_values.append(min_delay)
+        fixed_s = [float(si) for si in sol(s)]
+
+        print fixed_s
+
+        P, q, G, h, A, b = p.build_path_qp(fixed_s)
+        sol = cvx.solvers.qp(2 * P, q, G, h, A, b)
+        l_gp = [max(epsilon, x) for x in sol['x']]
+                
+        # # print min_delay, "opt s", fixed_s
+        M, s, l = p.build_path_ggp(mode=DELAY_MIN, fixed_s = fixed_s)
+        sol = M.localsolve(verbosity=0)
+        min_delay = float(sol["cost"])
+        # # # print "Optimised l"
+        # # print "Minimum delay is: " + str(min_delay) + " with power " + str(float(sum(sol(s))))
+        l_qp = [float(x) for x in sol(l)]
+        # print "sum l: ", sum(sol(l))
+        
+        print "QP / GP:"
+        dif = [abs(l_qp[i] - l_gp[i]) for i in range(len(l_qp))]
+        print l_gp, p.get_delay(fixed_s, l_gp)
+        print l_qp, p.get_delay(fixed_s, l_qp)
+        print dif
+        print sum(dif)
+
+    print ggp_sol, sol_values[-1]
+
+    # print sol_values
+    # print fixed_l
+    # print fixed_s
+
+def alternate_gp_qp(n, m):
+    p = Path(n)
+
+    p.length = n / 2.
+
+    p_in = 5.
+    p_out = round(random.random() * 10 + 1, 0)
+
+    p.wire_res = 2 #random.random() * 2.
+    p.wire_cap = 2 #random.random() * 2.
+
+    print "Initializing constants:"
+    print p_in, p_out, p.wire_cap, p.wire_res, p.gate_cap, p.wire_cap
+
+    print "Finding GGP solution..."
+    start = timer()
+    M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = p_in, prim_out = p_out)
+    sol = M.localsolve(verbosity=0)
+    min_delay = float(sol["cost"])
+    print "Found GGP solution in", timer() - start, "seconds."
+
+    ggp_sol = min_delay
+    ggp_s = [float(si) for si in sol(s)]
+    ggp_l = [float(li) for li in sol(l)]
+    
+    sol_values = []
+
+    epsilon = 0.000000000001
+
+    print "Performing ACS (" + str(m) + " iterations )." 
+    
+    fixed_l = random_l(n - 1, p.length)
+    fixed_l = uniform_l(n - 1, p.length)
+
+    print "Initial l:"
+    print fixed_l
+
+    start = timer()
+    for i in xrange(m):
+        M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = p_in, prim_out = p_out, fixed_l = fixed_l)
+        sol = M.solve(verbosity=0)
+        min_delay = float(sol["cost"])
+        # print "Optimised s"
+        # print "Minimum delay is: " + str(min_delay) + " with power " + str(float(sum(sol(s))))
+        # print "s:     ", sol(s)
+        # print "l:     ", sol(l)
+        # print "sum l: ", sum(sol(l))
+
+        sol_values.append(min_delay)
+        fixed_s = [float(si) for si in sol(s)]
+
+        P, q, G, h, A, b = p.build_path_qp(fixed_s)
+        sol = cvx.solvers.qp(2 * P, q, G, h, A, b)
+        fixed_l = [max(epsilon, x) for x in sol['x']]
+        sol_values.append(p.get_delay(fixed_s, fixed_l))
+
+    print "Finished ACS in", timer() - start, "seconds."
+
+    print "GGP solution:"
+    print "d:     ", ggp_sol
+    print "s:     ", ggp_s
+    print "l:     ", ggp_l
+
+    print "ACS solution:"
+    print "d:     ", min_delay
+    print "s:     ", fixed_s
+    print "l:     ", fixed_l
+
+    rel_sol_values = [v / ggp_sol for v in sol_values]
+
+    plt.plot(rel_sol_values)
+
+    # if ggp_sol * (1. + epsilon) < sol_values[-1] or ggp_sol > sol_values[-1] * (1. + epsilon):
+    #     print p_in, p_out, p.wire_cap, p.wire_res
+
+
 if __name__=="__main__":
-    for i in range(int(sys.argv[1])):
-        print "Run", i
-        n=int(sys.argv[2])
-        make_run(n)
+    n = int(sys.argv[1])
+    m = int(sys.argv[2])
+    k = int(sys.argv[3])
+    
+    for i in range(k):
+        alternate(n, m)
+        # make_run(n)
+    # plt.show()
+
+    # for i in range(int(sys.argv[1])):
+    #     print "Run", i
+    #     n=int(sys.argv[2])
+    #     make_run(n)
     
     # # Compute minimum delay.
     # M, s, l = p.build_path_ggp(mode=DELAY_MIN, prim_in = 5., prim_out = 1.)
